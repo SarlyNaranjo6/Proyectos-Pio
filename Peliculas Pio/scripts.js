08b2c574bffd3035df55cd9a99c1e3758dea8dd2
SarlyNaranjo6 <sarlynaranjo26@gmail.com>
2024-10-14 19:30:50 -0500

    Peliculas

---
+++ Peliculas Pio/scripts.js
@@ -0,0 +1,15 @@
+function validarFormulario(){
+    var nombre = document.getElementById("nombre").value;
+
+    if (nombre === ""){
+        alert("Por favor, ingresa tu nombre.")
+        return false
+    }
+    alert("Formulario enviado con exito.")
+    return true
+}
+
+document.getElementById("cambiarColor").onclick = function () {
+    var titulo = document.getElementById("titulo");
+    titulo.style.color = titulo.style.color == 'red' ? '#4CAF50' : 'red';
+}
